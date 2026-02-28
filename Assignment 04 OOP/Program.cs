1184e47138d283e63a16d55f8792b7b014ab28ff
mohamedkhaledabdelfattah23 <mohamedkhaledabdelfattah23@gmail.com>
2026-02-28 23:23:39 +0200

    PART 01

--- Assignment 04 OOP/Program.cs
+++ Assignment 04 OOP/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace Assignment_04_OOP
+﻿using System.Numerics;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment_04_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
+
+            #region PART 01
+            //// Part 01
+
+
+            //  Question 01 :
+
+            //          Static Binding(Early Binding) 
+            //                 =>  compile time
+
+
+
+
+            //          Dynamic Binding(Late Binding)
+            //                    => run time
+
+
+
+
+
+            //  Question 02 :
+
+            //          Method Overloading : Same method name, different parameter lists — within the same class. Resolved at compile time.
+
+
+
+            //          Method Overriding : Derived class overrides a virtual method from the base class using the override keyword.
+
+
+
+
+            // Question 03 :
+
+
+
+            //            virtual → Allows a method to be override in a child class.
+
+            //            override → Used in a child class to redefine a method.
+
+            //            sealed → Prevents any other class from overriding it.
+
+            #endregion
+
+
+
+
+
+
+
+
         }
     }
 }
